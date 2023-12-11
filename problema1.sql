a23782f8d78336569723868f9b326578cc32084e
methalox <101787603+methalox@users.noreply.github.com>
2023-12-11 08:10:32 +0100

    Add files via upload

---
+++ problema1.sql
@@ -0,0 +1,29 @@
+-- Devolver el número de Pokémon que ha capturado cada entrenador
+SELECT t.id, t.name, COUNT(*)
+FROM trainer t
+JOIN trainer_pokedex tp ON t.id = tp.trainer_id
+GROUP BY t.id, t.name;
+
+-- Devolver los pokémon de tipo Electric que haya capturado Ash Ketchum
+SELECT p.id, p.level, ps.name, tp.captured_at
+FROM pokemon p
+JOIN pokemon_species ps ON p.pokemon_species_id = ps.id
+JOIN trainer_pokedex tp ON p.id = tp.pokemon_id
+JOIN trainer t ON tp.trainer_id = t.id
+JOIN pokemon_species_type pst ON ps.id = pst.pokemon_species_id
+JOIN pokemon_type pt ON pst.pokemon_type = pt.type
+WHERE t.name = 'Ash Ketchum'
+AND pt.type = 'Electric';
+
+-- Devolver último pokémon que haya capturado Brock
+SELECT p.id, p.level, ps.name, tp.captured_at
+FROM pokemon p
+JOIN pokemon_species ps ON p.pokemon_species_id = ps.id
+JOIN trainer_pokedex tp ON p.id = tp.pokemon_id
+JOIN trainer t ON tp.trainer_id = t.id
+WHERE t.name = 'Brock'
+AND tp.captured_at = (
+    SELECT MAX(captured_at)
+    FROM trainer_pokedex
+    WHERE trainer_id = t.id
+);
